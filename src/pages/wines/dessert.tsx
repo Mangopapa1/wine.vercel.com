b9d16d49c2f02c7e5dc3d2be14c27f528b7e8e37
Hyunse Ryu <gustp26@naver.com>
2021-12-20 22:27:22 +0900

    속성 페이지 추가

---
+++ src/pages/wines/dessert.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+//import 가 머지?
+const WinePage: NextPage = () => {
+    return (
+        <div>
+            <h1>dessert</h1>
+      </div>
+    )
+}
+
+export default WinePage;
+//export 가 머지?
---
+++ src/pages/wines/port.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+//import 가 머지?
+const WinePage: NextPage = () => {
+    return (
+        <div>
+            <h1>tsx</h1>
+      </div>
+    )
+}
+
+export default WinePage;
+//export 가 머지?
