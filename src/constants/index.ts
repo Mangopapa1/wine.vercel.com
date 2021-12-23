ff5c1c313bcf11315d82316f75fbe89fe4d3758f
Mangopapa1 <gustp26@gmail.com>
2021-12-23 20:14:02 +0900

    💄WineCard Css 적용

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,9 @@
 export const WINE_API_ENDPOINT = 'https://api.sampleapis.com/wines/';
 export const BEER_API_ENDPOINT = 'https://api.sampleapis.com/beers/';
+//미디어쿼리 함수 만들기
+export const MEDIA_QUERY_END_POINT = {
+    MOBILE: '640px',
+    TABLET: '1280px',
+    DESKTOP: '1920px',
+    LARGE: '2560px'
+  }
